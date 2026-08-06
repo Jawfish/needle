d313f4d8fa6564ae4007390b135dde9c74f6a74a
James Fitzgerald <james@jawfish.dev>
2026-08-06 09:58:18 -0500

    package needle as a Nix flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,12 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { nixpkgs, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }:
     let
       systems = [
         "x86_64-linux"
@@ -12,9 +17,66 @@
         "aarch64-darwin"
       ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
+      needleFor =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.rustPlatform.buildRustPackage {
+          pname = "needle";
+          version = "0.4.0";
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "xberg-1.0.14" = "sha256-9WINAtQs74zRpCWGjB6lLDZDXIsXlIBLyqQJ+C0wKLU=";
+            };
+          };
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          nativeCheckInputs = [ pkgs.cacert ];
+          buildInputs = [
+            pkgs.onnxruntime
+            pkgs.openssl
+          ];
+          ORT_LIB_LOCATION = "${pkgs.onnxruntime}/lib";
+          ORT_PREFER_DYNAMIC_LINK = "1";
+          preCheck = ''
+            export HOME="$TMPDIR/home"
+            export XDG_CONFIG_HOME="$HOME/config"
+            export XDG_DATA_HOME="$HOME/data"
+            export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+            mkdir -p "$XDG_CONFIG_HOME" "$XDG_DATA_HOME"
+          '';
+          meta = {
+            description = "Local semantic search for documents";
+            homepage = "https://github.com/Jawfish/needle";
+            license = pkgs.lib.licenses.mit;
+            mainProgram = "needle";
+          };
+        };
     in
     {
-      devShells = forAllSystems (system:
+      packages = forAllSystems (
+        system:
+        let
+          needle = needleFor system;
+        in
+        {
+          inherit needle;
+          default = needle;
+        }
+      );
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.needle}/bin/needle";
+          meta.description = "Local semantic search for documents";
+        };
+      });
+
+      devShells = forAllSystems (
+        system:
         let
           pkgs = import nixpkgs { inherit system; };
         in
@@ -35,6 +97,7 @@
 
             LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
           };
-        });
+        }
+      );
     };
 }
